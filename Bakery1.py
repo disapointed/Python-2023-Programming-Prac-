f58bd50dfe88e478f10fb37858b126b4d87b1f11
disapointed <149742723+disapointed@users.noreply.github.com>
2023-11-03 12:26:18 +1100

    DescritiveDesserts

---
+++ Bakery1.py
@@ -0,0 +1,9 @@
+ask = input("Treat? ")
+if ask == "Pastry":
+    print("Buttery flaky goodness")
+elif ask == "Cake":
+    print("A light and fluffy classic")
+elif ask == "Cookie":
+    print("Warm and crispy on the edges")
+else:
+    print("We do not sell that. It sounds yum!")
